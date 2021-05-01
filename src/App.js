ec4220efff02740fe4da44e392cac0a7555daed0
MarieGaac <marielessgama@hotmail.com>
2021-05-01 13:53:05 -0500

    Contador con funcionalidad y estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Counter = (props) => {
+	return (
+		<div>
+			<span>Contador</span>
+			<h2>{props.count}</h2>
+		</div>
+	);
+}
+
+const Counter2 = (props) => {
+	return (
+		<div>
+			<span>Salto</span>
+			<h2>{props.count2}</h2>
+		</div>
+	); 
+}
+
+const App = () => {
+	const [count, setCount] = useState(0);
+
+	const [count2, setCount2] = useState(0);
+
+	return (
+	<>
+		<h1>Contador</h1>
+		<section>
+			<div>
+				<Counter count = {count}/>
+				<div>
+				<button onClick={() => setCount(count + count2)}>+1</button>
+				<button onClick={() => setCount(count - count2)}>-1</button>
+				<button onClick={() => setCount(0)}>Reset</button>
+				</div>
+			</div>
+			<div>
+				<Counter2 count2 = {count2}/>
+				<div>
+					<button onClick={() => setCount2(count2 + 1)}>+1</button>
+					<button onClick={() => setCount2(count2 - 1)}>-1</button>
+					<button onClick={() => setCount(0)}>Reset</button>
+				</div>
+			</div>
+		</section>
+	</>
+	);
 }
 
 export default App;
